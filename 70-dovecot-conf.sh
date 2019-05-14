86aa15024baa3ef4ea85c8a3664b4abc5c8fc4d5
Jesse Weisner <jesse_weisner@bcit.ca>
2019-05-14 12:37:20 -0700

    fixed overwrite issue with confdrop, protodrop

--- 70-dovecot-conf.sh
+++ 70-dovecot-conf.sh
@@ -1,3 +1,3 @@
 [ -f ${CONFDIR}/.keep ] || tar zxf /dovecot-conf.tar.gz -C ${CONFDIR}
-[ -f ${CONFDROPDIR/.keep} ] || tar zxf /dovecot-confdrop.tar.gz -C ${CONFDROPDIR}
-[ -f ${PROTODROPDIR/.keep} ] || tar zxf /dovecot-protocols.tar.gz -C ${PROTODROPDIR}
+[ -f ${CONFDROPDIR}/.keep ] || tar zxf /dovecot-confdrop.tar.gz -C ${CONFDROPDIR}
+[ -f ${PROTODROPDIR}/.keep ] || tar zxf /dovecot-protocols.tar.gz -C ${PROTODROPDIR}
