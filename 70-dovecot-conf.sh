b47672e078f2a7575d19b1ed2e5f27ac40023b48
Juraj Ontkanin <Juraj_Ontkanin@bcit.ca>
2019-07-11 15:09:12 -0700

    dockerize only .tmpl files

--- 70-dovecot-conf.sh
+++ 70-dovecot-conf.sh
@@ -3,9 +3,13 @@
 [ -f ${PROTODROPDIR}/.keep ] || tar zxf /dovecot-protocols.tar.gz -C ${PROTODROPDIR}
 
 if [ -f "$CONFDIR"/.DOCKERIZE.env ]; then
-    echo "loading ${CONFDIR}/.DOCKERIZE.env environment"
+    echo "loading: ${CONFDIR}/.DOCKERIZE.env"
     . "$CONFDIR"/.DOCKERIZE.env
 fi
-for config_file in $( find "${CONFDIR}" "${CONFDROPDIR}" "${PROTODROPDIR}" -type f ); do 
-    dockerize -template "$config_file":"$config_file"
+for tmpl_file in $( find "${CONFDIR}" "${CONFDROPDIR}" "${PROTODROPDIR}" -type f -name '*.tmpl' -not -path '*/\.git/*' ); do
+    config_file="$( dirname -- "$tmpl_file" )/$( basename -- "$tmpl_file" .tmpl )"
+    echo "dockerizing: ${tmpl_file}"
+    echo "         =>  ${config_file}"
+    dockerize -template "$tmpl_file":"$config_file" \
+    && rm -f "$tmpl_file"
 done
